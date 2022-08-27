56e4a475c01b2d1411379ce83ed8293a5514ecde
Kaushik0301-github-99 <kaushikkumar7190@gmail.com>
2022-08-27 17:52:06 +0530

    work done upto the subscribe

--- src/App.js
+++ src/App.js
@@ -3,24 +3,56 @@ import './App.css';
 import {data} from './data'
 import NavBar from './navBar'
 import MovieCard from './movieCard'
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    <NavBar/>
-    <div className='main'>
-      <div className='tabs'>
-        <div className='tab'>Movies</div>
-        <div className='tab'>favourites</div>
+
+// as we know that we should make a call to the api in component did mount adn to make a component did mount we will have to make a class component here
+
+
+
+class App extends React.Component {
+componentDidMount(){
+const {store} = this.props;
+
+  // make api call 
+
+  // step 2 subscribing to the state and subscribe takes a function 
+  store.subscribe(()=>{
+   console.log("UPDATED STATE")
+
+   //! force update method is update the whole ui app forcefully 
+   this.forceUpdate();
+  })
+
+  //! dispatch action => this action is use to send the data or to change the state last state 
+//! and we can see that even after dispatching the action our page state is not changing so we will have to subscribe to the state 
+  store.dispatch({
+    type:"ADD_MOVIES",
+    movies:data
+  })
+  console.log("movies added", this.props.store.getState())
+}
+
+  render() {
+    const movies = this.props.store.getState();
+    console.log("RENDERED")
+    return (
+      <div className="App">
+        <NavBar />
+        <div className='main'>
+          <div className='tabs'>
+            <div className='tab'>Movies</div>
+            <div className='tab'>favourites</div>
+          </div>
+          <div className='list'>
+            {movies.map((movie,index)=>{
+     return <MovieCard movie = {movie} key={index}></MovieCard>
+    })}
+          </div>
+        </div>
       </div>
-      <div className='list'>
-  {data.map((movie)=>{
-   return <MovieCard movie = {movie}></MovieCard>
-  })}
-      </div>
-    </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom/client';
-import { Provider } from 'react-redux';
 
+//! provider is use to use the store that we have created ---- sometimes we can even use without a provider
+// import { Provider } from 'react-redux';
+
+//! we are using the lagecy because it gives as a permission to use the before use content 
 import {legacy_createStore as createStore} from 'redux'
 
 
@@ -9,19 +12,30 @@ import './index.css';
 import App from './App';
 import movies from './reducer';
 
+// 
 const store = createStore(movies);
-console.log('store', store);
-console.log('state', store.getState());
+// console.log('store', store);
+// console.log(' before state', store.getState());
+
+// store.dispatch({
+//   type:"ADD_MOVIES",
+//   movies:[{name:"Abhishek bkchod"}]
+// })
+
+// console.log('after state',store.getState())
+
+
+//! one thing to notice that when we use the react strict mode the component get called two times then remember it 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Provider>
+  // <Provider  >
+  <>
+    <App store = {store}/>
+  </>
+  //  </Provider>
 );
 
 
